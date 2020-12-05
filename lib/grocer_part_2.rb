28c8d2f2976d4c77e8c68b9c6de6970a96fd6950
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-05 01:52:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,7 +8,7 @@ def apply_coupons(cart, coupon)
   while counter < coupon.length
     binding.pry
     cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    cart_item_w_coupon = "#{cart[counter][:item]}W/COUPON"
+    cart_item_w_coupon = "#{coupon[counter][:item]}W/COUPON"
     counter += 1
   end
 
